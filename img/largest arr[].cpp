ee98343a5812fa9a143fca7deb3575f735aeced0
_nibisa_ <nibisatimsina123@gmail.com>
2025-11-28 21:13:39 +0545

    Add files via upload

---
+++ img/largest arr[].cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+template<typename T> T Largest(T arr[],int n)
+{
+	T large=arr[0];
+	for(int i=0; i<=n; i++)
+	{
+		if(large<arr[i])
+		{
+			large=arr[i];
+		}
+		
+	}
+	return large;
+}
+int main()
+{
+	int int_arr[]={22333,2222,222};
+	cout << "Largest among all is:" << Largest(int_arr,3);
+	return 0;
+}
